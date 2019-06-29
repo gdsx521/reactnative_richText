94d38f4f208ca56ea3282ad3ee828020737c5fef
卜宇洱海~ <1073894748@qq.com>
2019-06-29 13:44:15 +0800

    update

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View,Image} from 'react-native';
 
 import MJRefresh, {ScrollView} from 'react-native-mjrefresh'//rn>=0.55
 import DJStyleTools from './Tools/DJStyleTools'
@@ -64,30 +64,46 @@ export default class App extends Component<Props> {
                 huanhang: 1,
             },
             {
-                title:'日照香炉生紫烟，遥看瀑布挂前川。飞流直下三千尺，疑是银河落九天。',
+                title: '日照香炉生紫烟，遥看瀑布挂前川。飞流直下三千尺，疑是银河落九天。日照香炉生紫烟，遥看瀑布挂前川。飞流直下三千尺，疑是银河落九天。日照香炉生紫烟，遥看瀑布挂前川。飞流直下三千尺，疑是银河落九天。日照香炉生紫烟，遥看瀑布挂前川。飞流直下三千尺，疑是银河落九天。',
                 titleColor: '#333',
                 fontSize: 15,
                 huanhang: 0,
             },
+
+            //添加一个图片
             {
-                title: '《望庐山瀑布》',
+                type: 1,
+                imageUrl: 'http://pics.mathfunc.com/item/25739.jpg',
+            },
+            {
+                title: '《早发白帝城》',
                 titleColor: 'red',
                 fontSize: 15,
                 huanhang: 1,
             },
-            // {
-            //     title:'日照香炉生紫烟，遥看瀑布挂前川。飞流直下三千尺，疑是银河落九天。',
-            //     titleColor: '#333',
-            //     fontSize: 15,
-            //     huanhang: 0,
-            // },
+            {
+                title: '朝辞白帝彩云间，千里江陵一日还。两岸猿声啼不住，轻舟已过万重山。朝辞白帝彩云间，千里江陵一日还。两岸猿声啼不住，轻舟已过万重山。朝辞白帝彩云间，千里江陵一日还。两岸猿声啼不住，轻舟已过万重山。朝辞白帝彩云间，千里江陵一日还。两岸猿声啼不住，轻舟已过万重山。',
+                titleColor: '#333',
+                fontSize: 15,
+                huanhang: 0,
+            },
 
         ]
 
-
+        //
         let textViewArr = []
-        objs.forEach((obj) => {
+        for (let index = 0; index < objs.length; index++) {
+            let obj = objs[index]
 
+            //1.首先判断是否是图片
+            let type = obj.type
+            if ((type != null) && type == 1) {
+                this._dealImageItemView(textViewArr, obj)
+                continue
+            }
+
+
+            //2.不是图片,则添加文字
             let titleStyle = {
                 fontSize: obj.fontSize,
                 color: obj.titleColor,
@@ -104,6 +120,7 @@ export default class App extends Component<Props> {
                     <QianTaoText
                         titleStyle={titleStyle}
                         title={obj.title}
+                        childView={[]}
                     />
                 )
             } else {
@@ -115,6 +132,7 @@ export default class App extends Component<Props> {
                         <QianTaoText
                             titleStyle={titleStyle}
                             title={obj.title}
+                            childView={[]}
                         />
                     )
                 } else {
@@ -138,12 +156,36 @@ export default class App extends Component<Props> {
                     textViewArr[textViewArr.length - 1] = newQianTaoText
                 }
             }
-        })
+        }
 
 
         return textViewArr.map((itemView) => {
             return itemView
         })
+    }
+
+
+    //获取图片视图
+    _dealImageItemView(textViewArr, obj) {
+       let imageUrl = obj.imageUrl
+        if (!imageUrl || imageUrl.length == 0){
+            return
+        }
+
+        let maxWidth = DJSizeTools.deviceWidth() - 30
+        let imageHeight = DJSizeTools.scaleSize(220)
+
+        let imageStyle={
+            width:maxWidth,
+            height: imageHeight,
+        }
+
+        let imageView = (
+            <Image source={{uri:imageUrl}} style={imageStyle}/>
+        )
+        textViewArr.push(imageView)
+
+
     }
 }
 
